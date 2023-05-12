2d88ad9b6349bd88595420e015e8de815a2bc5f2
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-13 00:27:58 +0200

    user input through console

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
+use std::io;
+
 fn main() {
     println!("uwu");
     let mut n = 0;
     n += 1;
     if n >= 0 {
         println!("amogus")
+    }
+    // Create a new instance of `std::io::stdin`
+    let mut input = String::new();
+
+    // Prompt the user for input
+    println!("Please enter your name:");
+
+    // Read the user's input
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    // Display the user's input
+    let trimmed_input = input.trim();
+
+    // Check if the input matches "uwu"
+    if trimmed_input == "uwu" {
+        println!("You entered uwu!");
+    } else {
+        // Display the user's input
+        println!("Hello, {}!", trimmed_input);
     }
 }
