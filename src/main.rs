e8b3a19c2a19e1ad094b283d1fad84a3a25baad0
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-16 21:27:46 +0200

    added magic eightball

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,67 @@ use std::io::{self, BufRead};
 use rand::Rng;
 
 fn main() {
+    println!("What would you like to do?");
+    println!("[Q]uiz? or [M]agic eight ball?");
+    let input = scan();
+    if input == "Q" {
+        quiz();
+    } else if input == "M" {
+        let question = "Am I cool?";
+        println!("{}", magic_eight_ball(question));
+    }
+}
+
+fn quiz() {
     let mut rng = rand::thread_rng();
 
     let random_int = rng.gen_range(-5..=0);
-    let questions: [&str; 3];
-    questions = ["Are turtles reptiles?", "Is rust an awesome language?", "UwU?"];
+    let questions: [&str; 3] = ["Are turtles reptiles?", "Is rust an awesome language?", "UwU?"];
     let mut score: i32 = 0;
     
     println!("{}", questions[0]);
-    if checkInput("Yes") == true {
+    if check_input("Yes") == true {
+        println!("Correct!");
         score += 1;
+    } else {
+        println!("Wrong!");
     }
 
     println!("{}", questions[1]);
-    if checkInput("Yes") == true {
+    if check_input("Yes") == true {
+        println!("Correct!");
         score += 1;
+    } else {
+        println!("Wrong!");
     }
     
     println!("{}", questions[2]);
-    if checkInput("UwU!") == true {
+    if check_input("UwU!") == true {
+        println!("Correct!");
         score += 1;
+    } else {
+        println!("Wrong!");
     }
 
     println!("{}", score);
     println!("Acshually you're score is: {}", random_int)
 }
 
-fn checkInput(solution: &str) -> bool {
-    #![allow(warnings)]
-    let input = scan().trim().to_owned();
+fn magic_eight_ball(question: &str) -> &str {
+    let mut rng = rand::thread_rng();
+    let random_int = rng.gen_range(0..=1);
+    if random_int == 0 {
+        return "Yes";
+    } else {
+        return "No";
+    }
+}
 
-    if input == "Yes" {
-        println!("Correct!");
+fn check_input(solution: &str) -> bool {
+    if scan() == solution {
         return true;
     } else {
         // Display the user's input
-        println!("Wrong!");
         return false;
     }
 }
@@ -48,5 +73,12 @@ fn scan() -> String {
         .lock()
         .read_line(&mut input)
         .expect("Failed to read line");
-    return input;
+    let trimmed_input = input.trim().to_owned();
+    return trimmed_input;
+}
+
+fn count_letters(text: &str) -> usize {
+    text.chars()
+        .filter(|c| c.is_alphabetic())
+        .count()
 }
